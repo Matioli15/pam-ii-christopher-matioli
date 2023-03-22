28c651dbb633009b878af2a27c2c2cd9d41974c7
Matioli15 <kifo.martins424@gmail.com>
2023-03-22 08:28:57 -0300

    fizemos variaveis e tipos em javascript

---
+++ aula02/variaveis.js
@@ -0,0 +1,28 @@
+let num1 = 1000
+const num2 = 4
+var msg = "MEU MANO VOCE TA SABENDO"
+
+
+console.log(msg, num1)
+console.log(num1 / num2)
+console.log(typeof num1)
+console.log(typeof msg)
+
+//JSON - JavaScript Object Notation
+
+let usuario = {
+    nome: "Derek",
+    idade: 29,
+    signo: "pocahontas",
+    bomnamusica: true
+}
+
+// console.log (typeof usuario)
+// console.log ("nome:", usuario.nome, "\n idade:", usuario.idade)
+// console,log (typeof usuario.bomnamusica)
+
+const nomes = ["Joao", "Oaoj"]
+
+console.log(nomes)
+console.log(typeof nomes)
+console.log(nomes[0])
